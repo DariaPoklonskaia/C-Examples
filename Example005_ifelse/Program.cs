e73d889b66b71d656c21e3a9013eeb0ee298936b
Daria <skotnikovada@mail.ru>
2022-06-05 23:59:27 +0300

    example 005 if else

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Inter username ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("EEE, this is Masha");
+}
+else
+{
+    Console.Write("Hi,");
+    Console.WriteLine(username);
+}
