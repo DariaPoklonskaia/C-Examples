36c2bb1c49fa306820cf6d0f6e1b08faa2822dd3
Daria <skotnikovada@mail.ru>
2022-06-06 22:30:41 +0300

    Added seminar examples

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+int num; 
+Console.Write("Input integer number ");
+num = Convert.ToInt32 (Console.ReadLine());
+
+num = num * num;
+Console.WriteLine("My results is " + num); 
+*/
+
+/*
+int num1, num2; 
+Console.Write("Input integer num1 ");
+num1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write("Input integer num2 ");
+num2 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+/* Example with cycle
+
+int num;
+Console.Write("Input integer number ");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++; 
+}
+*/
