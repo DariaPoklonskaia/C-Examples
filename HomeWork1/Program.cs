29f0d80a889b21211235c389104b0a01974c9fa9
Daria <skotnikovada@mail.ru>
2022-06-07 22:55:59 +0300

    First Ex of HW1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ex1
+/*
+int num1, num2;
+
+Console.WriteLine("Inter first integer number ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inter second integer number ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 >= num2)
+{
+    Console.WriteLine("max is " + num1);
+}
+else 
+{
+    Console.WriteLine("max is " + num2);
+}
+*/
