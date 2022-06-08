16c5b94f259e15c5c5ef2a7867c1c3d5fac71455
Daria <skotnikovada@mail.ru>
2022-06-08 15:39:28 +0300

    further modifications of Ex 010

--- Example010_ArrayLibrary/Program.cs
+++ Example010_ArrayLibrary/Program.cs
@@ -15,12 +15,36 @@ void PrintArray(int[] col) // void non-return method
     int position = 0;
     while (position < count)
     {
-        Console.WriteLine(col[position]);
+        Console.Write(col[position] + " ");
         position++;
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0; 
+    int position = -1; //if there is no such element, -1 would be interpreted as such element is not founded. 
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break; 
+        }
+        index++;
+    }
+    return position;
+}
+
 int [] array = new int[10]; //create new array with 10 elements; by default all elements would be 0. above to avoid all 0 in the array
 
-FillArray(array);
-PrintArray(array);
+FillArray(array); // call method to fill array
+array[4] = 4;
+array[6] = 4;
+PrintArray(array); // call method to print 
+Console.WriteLine();
+
+int pos = IndexOf(array,4);
+Console.WriteLine(pos);
