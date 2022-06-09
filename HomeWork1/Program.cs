fd76e63130016d5f29ac5e64b365e43d0ffc9a00
Daria <skotnikovada@mail.ru>
2022-06-09 10:10:17 +0300

    Second ex in HW added

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -18,3 +18,33 @@ else
     Console.WriteLine("max is " + num2);
 }
 */
+
+//Ex 2
+/*
+int num1, num2, num3;
+
+Console.WriteLine("Inter first integer number ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inter second integer number ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inter third integer number ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) 
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+
+Console.WriteLine("max is " + max);
+*/ 
+
+// Ex 3
+
