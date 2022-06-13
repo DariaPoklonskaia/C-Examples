fc75149b8499893aaa436c89be225d49e746a1b5
Daria <skotnikovada@mail.ru>
2022-06-13 15:11:16 +0300

    Third ex is done

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -24,7 +24,7 @@ Console.WriteLine(CutNumber(usernum));
 
 
 //Ex 2
-
+/*
 string CutNumber(int number)
 {
     if (number>99)
@@ -45,3 +45,46 @@ Console.WriteLine("Input number ");
 int usernum = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine(CutNumber(usernum));
+
+*/
+
+// Ex 3 (var1)
+
+/*
+string CheckWeekends(int day)
+{
+    if (day >5 && day<8)
+        {
+            string yes = "Yes";
+            return yes;
+        }
+    if (day > 0 && day < 6)
+        {
+             string no = "NO";
+            return no;
+        }
+    else 
+        {
+            string notAweekand = "There are only 7 days in a week, enter number from 1 to 7";
+            return notAweekand;
+        }
+}
+
+Console.WriteLine("Inter day of the week from 1 to 7, where 1 - Monday and 7 - Sunday");
+int userDay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CheckWeekends(userDay));
+
+*/ 
+
+//Ex3 (var2)
+
+bool CheckWeekends(int day)
+{
+    return (day >5 && day<8);
+}
+
+Console.WriteLine("Inter day of the week from 1 to 7, where 1 - Monday and 7 - Sunday");
+int userDay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CheckWeekends(userDay));
