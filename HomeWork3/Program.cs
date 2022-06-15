a71bab0aca975596a56aa8ccc8bae0b29fe9951a
Daria <skotnikovada@mail.ru>
2022-06-15 15:56:08 +0300

    Ex1 HW3 in progress

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string CheckPalindrom (int num)
+{
+    if (num > 9999 && num < 10^5)
+        {
+            if (num/10^4 == num%10 && num/10^3 == num % 100)
+            return yes = "Yes";
+            else return no = "No";
+        }
+    else return NotValid = "You bumber is not 5-digital";
+}
+
+Console.WriteLine("Inter 5-digital number"); 
+int userNum = Convert.ToInt32(read)
---
+++ HomeWork3UseThisOne/Program.cs
@@ -0,0 +1,18 @@
+﻿string CheckPalindrom(int num)
+{
+    if(num / 10000 == num % 10 && num/1000 == num%100)
+        {
+            string yes = "yes";
+            return yes;
+        }
+    else
+        {
+            string no = "no";
+            return no;
+        }
+}
+
+Console.WriteLine("input 5-digital number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CheckPalindrom(userNum));
