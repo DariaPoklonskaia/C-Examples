908ea5dc1265af022a3df79c5b15047b8787da36
Daria <skotnikovada@mail.ru>
2022-06-18 09:45:00 +0300

    Ex3-4 in seminar4 added

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -26,7 +26,7 @@ int FindSumOfDigits (int num)
     int count = 1;
     while (count <= Math.Abs(num))
         {
-            sum = sum + count; // short form sum += num%10 (+= увеличить, -= уменьшить, *= увеличить в несколько раз, /= сократить в несколько раз)
+            sum = sum + count; 
             count++; 
         }
     if (num < 0) return sum = sum * -1;
@@ -39,5 +39,40 @@ int userNum = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(FindSumOfDigits(userNum));
 */
 // Ex3 принимает на вход число и возращает произведение чисел от 1 до этого числа.
+/*
+int Find(int num)
+{
+    int prod = 1; 
+    int count = 1;
+    while (count <= Math.Abs(num))
+        {
+            prod = prod*count; 
+            count++; 
+        }
+    if (num < 0 && num%2 ==1) return prod = prod * -1;
+    else return prod;
+}
 
+Console.WriteLine("Input number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Find(userNum));
+*/
 // Ex4 принимает число и возвращает количество цифр в этом числе. 
+
+int FindNofDigits (int num)
+{
+    int count = 0; 
+    num = Math.Abs(num);
+    while (num > 0)
+        {
+            num = num/10; 
+            count++;
+        }
+        return count;
+}
+
+Console.WriteLine("Input number");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FindNofDigits(userNum));
